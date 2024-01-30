034133e475132a6650a4241e152dcd22d6f543d3
dianp <dprasmail@gmail.com>
2024-01-30 12:49:42 +0700

    table component with props

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,23 @@
 // import { useState } from "react";
+// import FormBelanja from "./components/FormBelanja/FormBelanja";
+import TabelHargaBuah from "./components/TabelHargaBuah/TabelHargaBuah";
 import "./index.css";
-import styles from "./css/Form.module.css";
+
+let dataHargaBuah = [
+  { nama: "Semangka", harga: 10000, berat: 1000 },
+  { nama: "Anggur", harga: 40000, berat: 500 },
+  { nama: "Strawberry", harga: 30000, berat: 400 },
+  { nama: "Jeruk", harga: 30000, berat: 1000 },
+  { nama: "Mangga", harga: 30000, berat: 500 },
+];
 
 function App() {
   // const [count, setCount] = useState(0);
 
   return (
     <>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          width: "100vw",
-          height: "100dvh",
-        }}
-      >
-        <div>
-          <form className={styles.form_card}>
-            <h1 className={styles.h1}>Form Pembelian Buah</h1>
-            <div className={styles.input_row}>
-              <label htmlFor="nama" className={styles.input_label}>
-                Nama Pelanggan
-              </label>
-              <input type="text" id="nama" name="nama" required />
-            </div>
-            <div className={styles.input_row}>
-              <label className={styles.input_label}>Daftar Buah</label>
-              <div className={styles.input_checkbox}>
-                <input type="checkbox" name="semangka" id="semangka" />
-                <label htmlFor="semangka">Semangka</label>
-              </div>
-              <div className={styles.input_checkbox}>
-                <input type="checkbox" name="jeruk" id="jeruk" />
-                <label htmlFor="jeruk">Jeruk</label>
-              </div>
-              <div className={styles.input_checkbox}>
-                <input type="checkbox" name="nanas" id="nanas" />
-                <label htmlFor="nanas">Nanas</label>
-              </div>
-              <div className={styles.input_checkbox}>
-                <input type="checkbox" name="salak" id="salak" />
-                <label htmlFor="salak">Salak</label>
-              </div>
-              <div className={styles.input_checkbox}>
-                <input type="checkbox" name="anggur" id="anggur" />
-                <label htmlFor="anggur">Anggur</label>
-              </div>
-            </div>
-            <div className={styles.input_row}>
-              <button type="submit" className={styles.button_submit}>
-                Beli
-              </button>
-            </div>
-          </form>
-        </div>
-      </div>
+      {/* <FormBelanja /> */}
+      <TabelHargaBuah dataHargaBuah={dataHargaBuah} />
     </>
   );
 }
